91f87605d0f4ee73bf5139b9d9c5c731f7af2c68
hchend2 <hervechendjou6@gmail.com>
2025-06-10 19:35:22 -0500

    implement user's schema

---
+++ user-auth-backend/server/models/User.js
@@ -0,0 +1,31 @@
+// server/models/User.js
+
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
